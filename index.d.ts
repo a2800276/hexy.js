a782175ec1f7656855c12a58ed1a85350a99fc10
Andrew Smithson <smithson@uk.ibm.com>
2020-08-10 23:07:46 +0100

    Update index.d.ts for latest typescript

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
-
-declare module "hexyz" {
+declare namespace hexyz {
   type FormatOptions = {
     width?: number;
     numbering?: "hex_bytes" | "none";
@@ -12,6 +11,7 @@ declare module "hexyz" {
     offset?: number;
     length?: number;
     display_offset?: number;
-  }
-  export const hexyz: (arg: Buffer | string | number[], format?: FormatOptions) => string;
+  };
 }
+
+export function hexyz(arg: Buffer | string | number[], format?: hexyz.FormatOptions): string;
